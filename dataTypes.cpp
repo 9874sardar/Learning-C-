056e1a67105a6f360d18bd67c62d6df49fe69fd5
9874sardar <69082943+9874sardar@users.noreply.github.com>
2022-08-11 11:54:11 +0530

    some code of c++ added

---
+++ dataTypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //short length
+    short a;
+    int a1;
+    long a2;
+    long long a3;
+    float a4;
+
+    // some more with float , double , long double
+    double c = 12;
+    long double c1 = 222.33;
+
+    // constant declartion
+    short const b=9;
+    float const b1 = 90.00;
+
+    // string
+    string t = "tariq";
+
+    return 0;
+}
---
+++ userInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b , sum ;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >>b;
+    sum = a + b;
+    cout << "The sum of two number is " << sum << endl; 
+
+    cout << "The muliplication of two number " << a*b << endl;
+    //Type Casting
+    cout << "The divison of two number " <<(float) a/b << endl;
+
+    return 0;
+}
